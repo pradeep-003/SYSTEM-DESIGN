b8977fe9a6cfcde8f517251c8f9f204721e48441
Pradeep-003 <pradeep50953@gmail.com>
2025-05-17 23:23:48 +0530

    SOL

--- src/Abstraction/Abstraction.java
+++ src/Abstraction/Abstraction.java
@@ -1,16 +1,17 @@
+package Abstraction;
 
 /*
-Car Interface --> Act as an interface for Outside world to operate the car.
+Abstraction.Car Interface --> Act as an interface for Outside world to operate the car.
 This interface tells 'WHAT' all it can do rather than 'HOW' it does that.
 Since this is an interface we cannot directly create Objects of this. We
 need to implement it first and then that child class will have the responsibility to
 provide implementation details of all the methods in the interface.
 
-In our real world example of Car, imagine you sitting in the car and able to operate
+In our real world example of Abstraction.Car, imagine you sitting in the car and able to operate
 the car (startEngine, accelerate, brake, turn) just by pressing or moving some
-pedals/buttons/stearing wheel etc. You dont need to know how these things work, and
-also they are hidden under thre hood.
-This Interface 'Car' denotes that (pedals/buttons/stearing wheel etc).
+pedals/buttons/stearing wheel etc. You don't need to know how these things work, and
+also they are hidden under their hood.
+This Interface 'Abstraction.Car' denotes that (pedals/buttons/stearing wheel etc).
 */
 interface Car {
     void startEngine();
@@ -22,16 +23,16 @@ interface Car {
 
 /*
 This is a Concrete class (A class that provide implementation details of an interface/abstract class).
-Now anyone can make an Object of 'SportsCar' and can assign it to 'Car' reference.
+Now anyone can make an Object of 'Abstraction.SportsCar' and can assign it to 'Abstraction.Car' reference.
 (See main method for this)
 
-In our real world example of Car, as you cannot have a real car by just having its body only
+In our real world example of Abstraction.Car, as you cannot have a real car by just having its body only
 (all these buttons or pedals). You need to have the actual implementation of 'What' happens
-when we press these buttons. 'SportsCar' class denotes that actual implementation.
+when we press these buttons. 'Abstraction.SportsCar' class denotes that actual implementation.
 
-Hence we can concude, to denote a real world car in programming we created 2 classes.
-One to deonte all the user-interface like pedals, buttons, stearing wheels etc ('Car' interface).
-And another one to denote the actual car with all the implementations of these buttons (SportsCar' class).
+Hence, we can conclude, to denote a real world car in programming we created 2 classes.
+One to denote all the user-interface like pedals, buttons, stearing wheels etc ('Abstraction.Car' interface).
+And another one to denote the actual car with all the implementations of these buttons (Abstraction.SportsCar' class).
  */
 class SportsCar implements Car {
     String brand;
@@ -54,7 +55,7 @@ class SportsCar implements Car {
     @Override
     public void shiftGear(int gear) {
         if (!isEngineOn) {
-            System.out.println(brand + " " + model + " : Engine is off! Cannot Cannot Shift Gear.");
+            System.out.println(brand + " " + model + " : Engine is off! Can't  Shift Gear.");
             return;
         }
         this.currentGear = gear;
